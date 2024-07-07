2c7408670c52396566bbff35c77db49b35327621
Priit Trees <trees@neti.ee>
2024-07-07 22:58:20 +0000

    panfrost: change MTX_SPIN mutexes to MTX_DEF.
    Tested with glxgears(1).

--- sys/dev/drm/panfrost/panfrost_drv.c
+++ sys/dev/drm/panfrost/panfrost_drv.c
@@ -770,7 +770,7 @@ panfrost_attach(device_t dev)
 		return (ENXIO);
 	}
 
-	mtx_init(&sc->as_mtx, "asid set mtx", NULL, MTX_SPIN);
+	mtx_init(&sc->as_mtx, "asid set mtx", NULL, MTX_DEF);
 
 	config_intrhook_oneshot(&panfrost_irq_hook, sc);
 
