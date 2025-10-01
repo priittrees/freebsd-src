16f0f9561f28b141a8e395cdfac5f4f159bae53c
Priit Trees <trees@neti.ee>
2025-10-01 18:00:48 +0000

    added mtk_mmc files

--- sys/arm64/mediatek/mtk_mmc.c
+++ sys/arm64/mediatek/mtk_mmc.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2019 - 2021 Priit Trees <trees@neti.ee>
+ * Copyright (c) 2019 - 2025 Priit Trees <trees@neti.ee>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -39,8 +39,8 @@
 
 #include <machine/bus.h>
 
-#include <dev/clk/clk.h>
-#include <dev/regulator/regulator.h>
+#include <dev/extres/clk/clk.h>
+#include <dev/extres/regulator/regulator.h>
 
 #include <dev/ofw/ofw_bus.h>
 #include <dev/ofw/ofw_bus_subr.h>
@@ -50,7 +50,7 @@
 #include <dev/mmc/mmcbrvar.h>
 #include <dev/mmc/mmc_fdt_helpers.h>
 
-#include <arm64/mediatek/mtk_mmc.h>
+#include <arm/mediatek/mtk_mmc.h>
 // #include <mips/mediatek/mtk_soc.h>
 // #include <mips/mediatek/mtk_sysctl.h>
 
@@ -217,10 +217,10 @@ mtk_mmc_attach(device_t dev)
 	//	goto fail;
 	//}
 
-	if (clk_get_by_ofw_name(dev, 0, "hclk", &sc->hclk)) {
-		device_printf(dev, "cannot get clock\n");
-		goto fail;
-	}
+	//if (clk_get_by_ofw_name(dev, 0, "hclk", &sc->hclk)) {
+	//	device_printf(dev, "cannot get clock\n");
+	//	goto fail;
+	//}
 
 	sc->sc_host.f_max = 25000000;
 	sc->sc_host.f_min = 260000;
@@ -983,36 +983,33 @@ static int
 mtk_mmc_config_clock(struct mtk_mmc_softc *sc, uint32_t freq)
 {
 
-	uint32_t sclk;
+//	uint32_t sclk;
 	uint64_t hclk;
 	uint32_t div;
 	int mode;
 	uint32_t val;
 
-	clk_get_freq(sc->hclk, &hclk);
-//	hclk = 800000000;
+//	clk_get_freq(sc->hclk, &hclk);
+	hclk = 800000000;
 
 	if (freq >= hclk) {
 		/* Use msdc source clock as bus clock */
 		mode = 1;
 		div  = 0;
-		sclk = hclk;
+//		sclk = hclk;
 	} else {
 		/* Use clock divider msdc source clock */
 		mode = 0;
 		if (freq >= (hclk >> 1)) {
 			/* divider 1/2 */
 			div = 0;
-			sclk = hclk >> 1;
+			//sclk = hclk >> 1;
 		} else {
 			/* divider 1/(n * 4) n: 1 - 255 */
 			div = (hclk + ((freq << 2) - 1)) / (freq << 2);
-			sclk = (hclk >> 2) / div;
+//			sclk = (hclk >> 2) / div;
 		}
 	}
-
-	device_printf(sc->sc_dev, "%s sclk %u hclk %lu freq %u div %u\n",
-		__func__, sclk, hclk, freq, div);
 
 	val = MTK_MMC_READ_4(sc, MTK_MSDC_CFG);
 	if (mode) {
